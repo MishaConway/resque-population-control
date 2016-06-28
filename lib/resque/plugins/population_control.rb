29f4af08de556899aa543487006a60adcef4698a
Misha Conway <mishaAconway@gmail.com>
2016-06-27 17:24:34 -0700

    initial checkin

---
+++ lib/resque/plugins/population_control.rb
@@ -0,0 +1,46 @@
+module Resque
+  module Plugins
+    module PopulationControl
+      VERSION = "0.1.0"
+
+      class PopulationExceeded < ::StandardError; end;
+
+      def population_control max
+        @population_control_max = max
+      end
+
+      def before_enqueue *args
+        population_control_count = population_control_increment
+        if population_control_count > population_control_max
+          population_control_decrement
+          raise PopulationExceeded
+          false
+        end
+      end
+
+      def after_perform *args
+        population_control_decrement
+      end
+
+      def population_control_max
+        @population_control_max
+      end
+
+      def population_control_increment
+         population_control_redis.incr population_control_cache_key
+      end
+
+      def population_control_decrement
+        population_control_redis.decr population_control_cache_key
+      end
+
+      def population_control_redis
+        @population_control_redis ||= Resque.redis.redis
+      end
+
+      def population_control_cache_key
+        "population_control_count_for_#{name}"
+      end
+    end
+  end
+end
